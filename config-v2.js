1f8a4eb00c2a551883586f7bfdee44859bf4e8ea
dansefunk1-lab <dansefunk1@gmail.com>
2026-03-31 20:13:38 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,5 +1,5 @@
 const DATABASE = "PRABATH-MD~dXujrnDcPFUi1HR" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
-const BOT_NUMBER = "0706000390" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
+const BOT_NUMBER = "94706000390" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
 const SESSION_ID = "PRABATH-MD~dXujrnDcPFUi1HR" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
 
